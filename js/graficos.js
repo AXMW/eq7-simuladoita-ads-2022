bef57de86907904bd49375c4f7012d84d1b77dba
Predo <lucasntavare000@gmail.com>
2022-11-11 23:51:35 -0300

    colocando botão de voltar pro início da pag estatisticas

--- js/graficos.js
+++ js/graficos.js
@@ -97,3 +97,20 @@ function drawResponses(question, answer, valuesAnswers) {
 for (let i = 0; i < allQuestions.length; i++) {
   drawResponses(allQuestions[i][0], allQuestions[i][1], allQuestions[i][2]);
 }
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
